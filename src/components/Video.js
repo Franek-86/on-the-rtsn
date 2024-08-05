b5e6197b907af0f44013b270afaca4f8207b7631
Franek-86 <fraleccese86@gmail.com>
2024-08-05 18:00:46 +0200

    test2

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,7 +4,7 @@ const Video = () => {
   const { stopData, test } = useGlobalContext();
   return (
     <div className='video-section'>
-      <iframe
+      {/* <iframe
         width='560'
         height='315'
         src={test ? stopData?.stop[0]?.video : "loading"}
@@ -13,6 +13,15 @@ const Video = () => {
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
         referrerpolicy='strict-origin-when-cross-origin'
         allowfullscreen
+      ></iframe> */}
+      <iframe
+        width='560'
+        height='315'
+        src='https://youtu.be/pbhncrh14xo'
+        title='YouTube video player'
+        frameborder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        referrerpolicy='strict-origin-when-cross-origin'
       ></iframe>
     </div>
   );
