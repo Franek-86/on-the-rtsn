2b89522e6aa721ab1887cd230508fabf5338115d
Franek-86 <fraleccese86@gmail.com>
2024-08-05 13:55:18 +0200

    test

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,13 +1,13 @@
 import React from "react";
-// import { useGlobalContext } from "../context";
+import { useGlobalContext } from "../context";
 const Video = () => {
-  // const { startJourney } = useGlobalContext();
+  const { stopData, test } = useGlobalContext();
   return (
     <div className='video-section'>
       <iframe
         width='560'
         height='315'
-        src='https://www.youtube.com/embed/pbhncrh14xo?si=wiNa_j6fcIAAt3RO'
+        src={test ? stopData?.stop[0]?.video : "loading"}
         title='YouTube video player'
         frameborder='0'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
